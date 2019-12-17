dd5b209098f034b2f53cea8e10507e5e13b3123d
bindal93 <shivam.bindal@yahoo.com>
2019-12-17 17:00:17 +0530

    http retry

--- src/app/book/book.service.ts
+++ src/app/book/book.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Book } from "./book";
+import { retry } from 'rxjs/operators';
 
 @Injectable()
 export class BookService {
   private booksUrl = "./assets/books.json";
   constructor(private http: HttpClient) {}
   getBooks(): Observable<Book[]> {
-    return this.http.get<Book[]>(this.booksUrl);
+    return this.http.get<Book[]>(this.booksUrl).pipe(retry(3));
   }
 }
