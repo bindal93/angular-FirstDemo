6c9401773ba7729e855bf7c74974dd71ea853a33
bindal93 <shivam.bindal@yahoo.com>
2019-12-18 15:18:11 +0530

    custom reactive validation

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -1,34 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-registration-form',
-  templateUrl: './registration-form.component.html',
-  styleUrls: ['./registration-form.component.css']
+  selector: "app-registration-form",
+  templateUrl: "./registration-form.component.html",
+  styleUrls: ["./registration-form.component.css"]
 })
 export class RegistrationFormComponent implements OnInit {
-registerForm: FormGroup;
+  registerForm: FormGroup;
 
-   constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) {}
   ngOnInit() {
-     this.registerForm = this.formBuilder.group({
-      firstName: ['',[Validators.required]],
-      lastName: ['', [Validators.required]],
+    this.registerForm = this.formBuilder.group({
+      firstName: ["", [Validators.required]],
+      lastName: ["", [Validators.required]],
       address: this.formBuilder.group({
-        street: [''],
-        zip: [''],
-        city: ['']
-      })
+        street: [""],
+        zip: [""],
+        city: [""]
+      }),
+      email: ["", validateEmail]
     });
   }
-   testing(){
-     debugger;
-     alert("inside testing :: "
-     +"firstName ::"+this.registerForm.get('firstName').value
-     +"lastName ::"+this.registerForm.get('lastName').value
-     +"address ::street "+this.registerForm.get('address').value.street
-     +" zip "+this.registerForm.get('address').value.zip
-     +" city "+this.registerForm.get('address').value.city);
-   }
-
+}
+function validateEmail(c: FormControl) {
+  let EMAIL_REGEXP = /^[\w._]+@[A-Za-z]+\.(com|co\.in|org)$/;
+  return EMAIL_REGEXP.test(c.value)
+    ? null
+    : {
+        message: "Email is invalid"
+      };
 }
